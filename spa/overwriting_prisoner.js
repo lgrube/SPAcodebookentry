fe47e35e29452ebb19facaec88b03200ed6bc679
lgrube <lgv9483@saintjoe.edu>
2015-09-08 07:49:58 -0500

    shows prototyping at work

---
+++ spa/overwriting_prisoner.js
@@ -0,0 +1,30 @@
+/*luke grube
+september 7 2015 page 41 in SPA book
+*/
+
+var proto = {
+  sentence : 4,
+  probation : 2
+};
+
+var makePrisoner = function (name, id) {
+  var prisoner = Object.create(proto);
+  prisoner.name = name;
+  prisoner.id = id;
+
+  return prisoner;
+};
+
+var firstPrisoner = makePrisoner('joe', '12A');
+
+console.log(firstPrisoner.sentence);
+console.log(firstPrisoner.__proto__.sentence);
+firstPrisoner.sentence = 10;
+
+console.log(firstPrisoner.sentence);
+
+console.log(firstPrisoner.__proto__.sentence);
+delete firstPrisoner.sentence;
+
+console.log(firstPrisoner.sentence);
+console.log(firstPrisoner.__proto__.sentence);
