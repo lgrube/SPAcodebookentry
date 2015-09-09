244901271392eeb1d0c90c3f73e249014047c299
lgrube <lgv9483@saintjoe.edu>
2015-09-09 18:17:06 -0500

    almost finished with chapter 3

--- spa/ch3_structure/js/spa.js
+++ spa/ch3_structure/js/spa.js
@@ -12,8 +12,9 @@
 /*global $, spa */
 
 var spa = (function () {
-  var initModule = function ($container) {
-    spa.shell.initModule($container);
+  var initModule = function ( $container ) {
+    spa.shell.initModule( $container );
   };
-  return {initModule: initModule};
+
+  return { initModule: initModule };
 }());
